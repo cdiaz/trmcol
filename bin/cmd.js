d81d9c2a2c9379dde5ed62dbe7f6e6fc012930ec
Diego Rodríguez <diegorbaquero@gmail.com>
2016-03-26 22:55:19 -0300

    Volver la fecha opcional, consola

---
+++ bin/cmd.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var trmcol = require('../')
+
+var d = new Date()
+d.setTime(d.getTime() + (d.getTimezoneOffset() - 300) * 60000) // Forzar hora colombiana
+
+trmcol.query(function (err, trm) {
+  if (err) {
+    console.error(err)
+    process.exit(0)
+  }
+  console.log('La TRM del día (' + d.toDateString() + ') es $' + trm.value)
+})
