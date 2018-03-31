a48e73f90fda576217dc6528110270f5b24acaed
Cristiam Diaz <c.diaz@udla.edu.co>
2018-03-31 11:33:28 -0500

    Fixes #7
    Refactorizar test

--- test/test.js
+++ test/test.js
@@ -1,40 +1,43 @@
 var test = require('tape')
-var trmcol = require('./../index')
+var trmcol = require('../src')
 
 test('Valor actual', function (t) {
-  t.plan(3)
+  t.plan(1)
 
-  trmcol.query(function (err, trm1) {
-    t.error(err)
-    var d = new Date()
-    d.setTime(d.getTime() - 300 * 60000) // Forzar hora colombiana
-    trmcol.query(d.toISOString().slice(0, 10), function (err, trm2) {
-      t.error(err)
-      t.equal(trm1.value, trm2.value)
+  trmcol.query()
+    .then(trm1 => {
+      var d = new Date()
+      d.setTime(d.getTime() - 300 * 60000) // Forzar hora colombiana
+      trmcol.query(d.toISOString().slice(0, 10))
+        .then(trm2 => {
+          t.equal(trm1.value, trm2.value)
+        })
+        .catch(err => {
+          t.error(err)
+        })
     })
-  })
 })
 
-test('Fechas Válidas', function (t) {
-  t.plan(4)
-
-  trmcol.query('2015-01-01', function (err, trm) {
-    t.error(err)
-    t.equal(trm.value, '2392.46')
-  })
-  trmcol.query('2016-01-01', function (err, trm) {
-    t.error(err)
-    t.equal(trm.value, '3149.47')
-  })
-})
-
-test('Fechas inválidas', function (t) {
+test('Fechas Válidas', (t) => {
   t.plan(2)
 
-  trmcol.query('15-01-01', function (err, trm) {
-    t.ok(err, 'Existe error')
-  })
-  trmcol.query('2016-13-01', function (err, trm) {
-    t.ok(err, 'Existe error')
-  })
+  trmcol.query('2015-01-01')
+    .then(trm => t.equal(trm.value, '2392.46'))
+    .catch(err => t.error(err))
+
+  trmcol.query('2016-01-01')
+    .then(trm => t.equal(trm.value, '3149.47'))
+    .catch(err => t.error(err))
+})
+
+test('Fechas inválidas', (t) => {
+  t.plan(2)
+
+  trmcol.query('15-01-01')
+    .then()
+    .catch(err => t.ok(err, 'Existe error'))
+
+  trmcol.query('2016-13-01')
+    .then()
+    .catch(err => t.ok(err, 'Existe error'))
 })
